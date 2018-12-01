558dc2f792b2a77ee371cf1f96f099d687e9dd10
dpimley <dpimley@purdue.edu>
2018-12-01 03:13:48 -0500

    initial commit with basic launch file

---
+++ devel/share/vesc_msgs/cmake/vesc_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(vesc_msgs_MESSAGE_FILES "/home/david/catkin_ws/src/vesc/vesc_msgs/msg/VescState.msg;/home/david/catkin_ws/src/vesc/vesc_msgs/msg/VescStateStamped.msg")
+set(vesc_msgs_SERVICE_FILES "")
