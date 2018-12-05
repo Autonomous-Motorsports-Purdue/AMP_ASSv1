47736cc65d4a1a051da55c68c568c861c72d2023
David Pimley <dpimley@purdue.edu>
2018-12-05 02:32:34 -0500

    removed devel folder

--- devel/share/vesc_msgs/cmake/vesc_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(vesc_msgs_MESSAGE_FILES "/home/david/catkin_ws/src/vesc/vesc_msgs/msg/VescState.msg;/home/david/catkin_ws/src/vesc/vesc_msgs/msg/VescStateStamped.msg")
-set(vesc_msgs_SERVICE_FILES "")
--- devel/share/vesc_msgs/cmake/vesc_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(vesc_msgs_MSG_INCLUDE_DIRS "/home/david/catkin_ws/src/vesc/vesc_msgs/msg")
-set(vesc_msgs_MSG_DEPENDENCIES std_msgs)
