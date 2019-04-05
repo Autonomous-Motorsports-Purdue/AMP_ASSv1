5a9eb8c11e913f8774c3810f35f7b6c468ecd0ec
Thomas Krause <krause24@purdue.edu>
2019-04-04 22:03:42 -0400

    Add files via upload

---
+++ src/AMP_ECU/util.h
@@ -0,0 +1,17 @@
+/*
+ * util.h
+ *
+ *  Created on: Mar 13, 2019
+ *      Author: David
+ */
+
+#ifndef SRC_UTIL_H_
+#define SRC_UTIL_H_
+
+typedef unsigned char       byte;
+typedef unsigned short      word;
+typedef unsigned int        dword;
+typedef signed char         int8_t;
+typedef unsigned char       uint8_t;
+
+#endif /* SRC_UTIL_H_ */
