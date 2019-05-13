10f96eb71965fcd28088dc1873d58425a2520ea7
NickScaturro <nscaturr@purdue.edu>
2019-05-13 16:48:54 -0400

    CMPSS stabilization

--- ti_code/CMPSS_test/main.c
+++ ti_code/CMPSS_test/main.c
@@ -73,6 +73,44 @@ void amp_comparator_initialize(void) {
     Cmpss2Regs.COMPCTL.bit.COMPDACE = 1;
     Cmpss5Regs.COMPCTL.bit.COMPDACE = 1;
 
+    //Initialize values for digital filter sample/threshold/clock pre-scale regs
+
+    /* CTRIPH Filter Control Register
+     * struct CTRIPHFILCTL_BITS {              // bits description
+        Uint16 rsvd1:4;                     // 3:0 Reserved
+        Uint16 SAMPWIN:5;                   // 8:4 Sample Window
+        Uint16 THRESH:5;                    // 13:9 Majority Voting Threshold
+        Uint16 rsvd2:1;                     // 14 Reserved
+        Uint16 FILINIT:1;                   // 15 Filter Initialization Bit
+    };
+     */
+    //
+    //Maximum SAMPWIN value provides largest number of samples
+    //
+    Cmpss2Regs.CTRIPHFILCTL.bit.SAMPWIN = 0x1F;
+    Cmpss5Regs.CTRIPHFILCTL.bit.SAMPWIN = 0x1F;
+
+    //
+    //Maximum THRESH value requires static value for entire window
+    //  THRESH should be GREATER than half of SAMPWIN
+    //
+    Cmpss2Regs.CTRIPHFILCTL.bit.THRESH = 0x1F;
+    Cmpss5Regs.CTRIPHFILCTL.bit.THRESH = 0x1F;
+
+    Cmpss2Regs.CTRIPHFILCTL.bit.FILINIT = 1;
+    Cmpss5Regs.CTRIPHFILCTL.bit.FILINIT = 1;
+
+    /* CTRIPH Filter Clock Control Register
+     * struct CTRIPHFILCLKCTL_BITS {           // bits description
+    Uint16 CLKPRESCALE:10;              // 9:0 Sample Clock Prescale
+    Uint16 rsvd1:6;                     // 15:10 Reserved
+    };
+     */
+    //Maximum CLKPRESCALE value provides the most time between samples
+    //
+    Cmpss2Regs.CTRIPHFILCLKCTL.bit.CLKPRESCALE = 0x3FF;
+    Cmpss5Regs.CTRIPHFILCLKCTL.bit.CLKPRESCALE = 0x3FF;
+
     //Specify that the inverting input is driven by internal DAC
     Cmpss2Regs.COMPCTL.bit.COMPHSOURCE = 0;
     Cmpss5Regs.COMPCTL.bit.COMPHSOURCE = 0;
@@ -98,6 +136,10 @@ void amp_comparator_initialize(void) {
     //Enable Mux for Output
     OutputXbarRegs.OUTPUT2MUXENABLE.bit.MUX2 = 1;
     OutputXbarRegs.OUTPUT4MUXENABLE.bit.MUX8 = 1;
+
+    //Histeresis
+    Cmpss2Regs.COMPHYSCTL.bit.COMPHYS = 0x2;
+    Cmpss5Regs.COMPHYSCTL.bit.COMPHYS = 0x2;
 
     //Disable write to set of registers
     EDIS;
