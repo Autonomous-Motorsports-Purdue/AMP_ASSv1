0d5e8bee9c6e46848d8e44674a6ae0793d2f7668
David Pimley <dpimley@purdue.edu>
2019-05-14 05:55:25 -0400

    added cmpss files

---
+++ src/AMP_ECU/amp_cmpss.h
@@ -0,0 +1,26 @@
+/*
+ * amp_cmpss.h
+ *
+ *  Created on: May 14, 2019
+ *      Author: David
+ */
+
+#ifndef AMP_CMPSS_H_
+#define AMP_CMPSS_H_
+
+#include "F28x_Project.h"
+#include "util.h"
+
+#include "amp_err.h"
+
+#include "amp_gpio.h"
+
+/* FUNCTION ---------------------------------------------------------------
+ * amp_err_code_t amp_cmpss_intialize();
+ *
+ * Initializes the current CMPSS to the specified defines above
+ */
+amp_err_code_t amp_pwm_initialize();
+
+
+#endif /* AMP_CMPSS_H_ */
