95bb64d1ed59d5d1d4d06c2694a2f3d35decbf3d
Zach Ghera <zpghera00@gmail.com>
2020-09-07 15:19:28 -0400

    kai_script unecessary and ROS re-install issues comment added to install dependency script

--- kai_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install ros-kinetic-teb-local-planner-tutorials
-
-source devel/setup.bash
-
-roscd teb_local_planner_tutorials && roslaunch launch/robot_carlike_in_stage.launch
-
-# 8=======D
