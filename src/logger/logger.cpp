6c5792a37e112de467fa1f913029d9441b1e0dae
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2022-02-06 12:31:40 -0500

    Add logger skeleton

---
+++ src/logger/logger.cpp
@@ -0,0 +1,16 @@
+// Serial logger class
+
+// Standard defines
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+// Ros defines
+#include "ros/ros.h"
+
+using namespace std;
+
+class Logger {
+
+};
